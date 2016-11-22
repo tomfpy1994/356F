bef10501d5ba244643fd21901576e66b6e8c912e
oliverlmc2016 <Oliver@Mande-MacBook-Air.local>
2016-11-22 22:05:33 +0800

    profile

---
+++ 356F 2/app/src/main/java/com/example/bunnyfung/a356f/NewPostPage.java
@@ -0,0 +1,13 @@
+package com.example.bunnyfung.a356f;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewPostPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post_page);
+    }
+}

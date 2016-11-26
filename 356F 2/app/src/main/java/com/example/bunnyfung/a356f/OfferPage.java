38e6a8c9fe6a7b84ae9d2089a1b55aa608769901
3dw1nH0 <lemonadeen@gmail.com>
2016-11-26 18:06:47 +0800

    Revert "Revert "OfferPage Done""
    This reverts commit 78043410467d0c0e794d0f6a86eae18bdf60a121.

---
+++ 356F 2/app/src/main/java/com/example/bunnyfung/a356f/OfferPage.java
@@ -0,0 +1,14 @@
+package com.example.bunnyfung.a356f;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OfferPage extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offer_page);
+    }
+}

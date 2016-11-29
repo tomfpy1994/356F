1e75744c687351b116b9ce070311ff1dfe632d61
3dw1nH0 <lemonadeen@gmail.com>
2016-11-29 22:43:17 +0800

    change the main.xml name to offer_page_row.xml

--- 356F 2/app/src/main/java/com/example/bunnyfung/a356f/OfferPage.java
+++ 356F 2/app/src/main/java/com/example/bunnyfung/a356f/OfferPage.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class OfferPage extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

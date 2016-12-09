0444a03d23c471372425c066d789a1e2912b37e1
oliverlmc2016 <Oliver@Mande-MacBook-Air.local>
2016-12-09 11:48:10 +0800

    9-12-2016

---
+++ 356F 2/app/src/main/java/com/example/bunnyfung/a356f/MyPostPage.java
@@ -0,0 +1,13 @@
+package com.example.bunnyfung.a356f;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyPostPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_post_page);
+    }
+}
---
+++ 356F 2/app/src/main/java/com/example/bunnyfung/a356f/ProfileHistoryPage.java
@@ -0,0 +1,13 @@
+package com.example.bunnyfung.a356f;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileHistoryPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_history_page);
+    }
+}

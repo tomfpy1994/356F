c447f5a5205e150b28bd4138a12ac7b2ab792ae1
oliverlmc2016 <Oliver@Mande-MacBook-Air.local>
2017-01-29 03:40:30 +0800

    new post hlaf

--- 356F 2/app/src/main/java/com/example/bunnyfung/a356f/FragNewPost.java
+++ 356F 2/app/src/main/java/com/example/bunnyfung/a356f/FragNewPost.java
@@ -3,9 +3,12 @@ package com.example.bunnyfung.a356f;
 import com.example.bunnyfung.a356f.Object.Account;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
@@ -13,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,23 +30,24 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class FragNewPost extends Fragment {
-    private Account acc;
-    private ImageView photo;
-    private TextView name, band, type, size, price, Description;
-    private Button submit, cancel;
-    private CheckBox halfSize;
+    public Account acc;
+    public ImageView photo;
+    public TextView name, band, type, size, price, description;
+    public Button submit, cancel;
+    public CheckBox halfSize;
     FragmentTransaction fragTransaction;
     Fragment frag;
-    private String stu ="";
+    public String stu ="";
+    public String sName, sBand, sType, sSize,sPrice, sDescription;
     private static final int SELECTED_PICTURE = 1;
-
+    View rootView;
 
     // constructor
     public FragNewPost(Account acc){this.acc=acc;}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_frag_new_post, null);
+        rootView = inflater.inflate(R.layout.fragment_frag_new_post, null);
 
         // test of get user information
         try{
@@ -58,10 +63,14 @@ public class FragNewPost extends Fragment {
         type = (TextView) rootView.findViewById(R.id.tfType);
         size = (TextView) rootView.findViewById(R.id.tfSize);
         price = (TextView) rootView.findViewById(R.id.tfPrice);
-        Description = (TextView) rootView.findViewById(R.id.tfDescription);
+        description = (TextView) rootView.findViewById(R.id.tfDescription);
         submit = (Button) rootView.findViewById(R.id.btnPostSubmit);
         cancel = (Button) rootView.findViewById(R.id.btnPostCancel);
         halfSize = (CheckBox) rootView.findViewById(R.id.cbSize);
+
+
+
+
 
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,25 +89,33 @@ public class FragNewPost extends Fragment {
             }
         });
 
+        halfSize.setOnCheckedChangeListener(new CheckBox.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+                if(isChecked){
+                    System.out.println("check box is select");
+                }
+            }
+
+        });
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    System.out.println(acc.passToJsonObjectStr());
-                    JSONObject jsonObject = new JSONObject(acc.passToJsonObjectStr());
-                    //Testing Log
-                    System.out.println("accString: " + acc.toString());
+                switch(view.getId()){
+                    case R.id.btnPostSubmit:
+                        //get user input
+                        sName = name.getText().toString();
+                        sBand = band.getText().toString();
+                        sType = type.getText().toString();
+                        sSize = size.getText().toString();
+                        sPrice = price.getText().toString();
+                        sDescription = description.getText().toString();
 
-                    doUpdate(jsonObject);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                while (stu.equals("")) {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+
+                        System.out.println(sName+" "+sBand+" "+sType+" "+sSize+" "+sPrice+" "+sDescription);
+
+
+
                 }
 
             }
@@ -109,70 +126,6 @@ public class FragNewPost extends Fragment {
     }
 
 
-    // not modify
-    public void doUpdate(final JSONObject acc) {
-        Thread thread = new Thread() {
-            public void run() {
-                StringBuilder sb = new StringBuilder();
-                HttpURLConnection connection = null;
-                try {
-                    URL url = new URL("http://s356fproject.mybluemix.net/api/updateac");
-                    connection = (HttpURLConnection) url.openConnection();
-                    connection.setDoOutput(true);
-                    connection.setDoInput(true);
-                    connection.setRequestMethod("POST");
-                    connection.setUseCaches(false);
-                    connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-                    connection.setConnectTimeout(10000);
-                    connection.setReadTimeout(10000);
 
-
-                    //Testing Log
-                    System.out.println("URL:" + url.toString());
-                    String strJsonobj = acc.toString();
-
-                    //Testing Log
-                    System.out.println("doLogin Method jsonObj: " + strJsonobj);
-
-                    OutputStream os = connection.getOutputStream();
-                    os.write(acc.toString().getBytes("UTF-8"));
-                    os.close();
-
-                    int HttpResult = connection.getResponseCode();
-
-                    //Testing Log
-                    System.out.println("resopnseCode: " + HttpResult);
-
-                    if (HttpResult == 200) {
-                        BufferedReader br = new BufferedReader(new InputStreamReader(
-                                connection.getInputStream()));
-                        String line = null;
-                        while ((line = br.readLine()) != null) {
-                            sb.append(line + "\n");
-                        }
-                        br.close();
-
-                        //Testing Log
-                        System.out.println("sb: " + sb.toString());
-
-                        JSONObject resultObject = new JSONObject(sb.toString());
-                        stu = resultObject.getString("status");
-
-                        //Testing Log
-                        System.out.println("responesStatud: "+stu);
-
-                    } else if (HttpResult == 402) {
-                        stu = "402";
-                    }
-
-                    connection.disconnect();
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        thread.start();
-    }
 
 }

859137f51b9ff120f41aac9502aab1a5a94af155
oliverlmc2016 <Oliver@Mande-MacBook-Air.local>
2017-01-28 12:36:28 +0800

    fixed

--- 356F 2/app/src/main/java/com/example/bunnyfung/a356f/FragMyProduct.java
+++ 356F 2/app/src/main/java/com/example/bunnyfung/a356f/FragMyProduct.java
@@ -10,36 +10,21 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-<<<<<<< HEAD
 import org.json.JSONException;
-=======
-public class FragMyProduct extends Fragment {
-    public ListView listView;
-    public ArrayAdapter<String> arrayAdapter;
-    View view;
-    String[] values = new String[]{
-            "Apple",
-            "Banana",
-            "Cat",
-            "Dog"
-    };
-    //contructor
-    public FragMyProduct() {}
->>>>>>> origin/master
 
 public class FragMyProduct extends Fragment {
     private ListView listView;
     private ArrayAdapter arrayAdapter;
     private Account acc = null;
     String[] values = new String[]{"one","two","three"};
+    // constructor
     public FragMyProduct() {}
     public FragMyProduct(Account acc){this.acc = acc;}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-<<<<<<< HEAD
         View rootView = inflater.inflate(R.layout.fragment_frag_my_product, null);
-        
+
         // testing of get user information
         try {
             System.out.println("FragMyProduct: "+acc.passToJsonObjectStr());
@@ -52,24 +37,13 @@ public class FragMyProduct extends Fragment {
         listView.setAdapter(arrayAdapter);
 
         // function of click item in list view
-=======
-        view = inflater.inflate(R.layout.fragment_frag_my_product, null);
-        listView = (ListView) view.findViewById(R.id.myPostList);
-        arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, values);
-        listView.setAdapter(arrayAdapter);
->>>>>>> origin/master
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(getContext(),parent.getItemAtPosition(position)+ " is select",Toast.LENGTH_LONG).show();
             }
         });
-<<<<<<< HEAD
         return rootView;
-=======
-        return view;
-
->>>>>>> origin/master
     }
 
 }

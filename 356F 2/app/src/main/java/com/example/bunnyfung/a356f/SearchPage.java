945dc948b14a901194a2bbe9edc73fb2ef7b37f2
oliverlmc2016 <Oliver@Mande-MacBook-Air.local>
2017-02-15 23:34:24 +0800

    search modify

--- 356F 2/app/src/main/java/com/example/bunnyfung/a356f/SearchPage.java
+++ 356F 2/app/src/main/java/com/example/bunnyfung/a356f/SearchPage.java
@@ -1,13 +1,76 @@
 package com.example.bunnyfung.a356f;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
 
 public class SearchPage extends AppCompatActivity {
-
+    private RadioGroup rgroup;
+    private RadioButton rb;
+    private TextView t,searchInput;
+    private Button btnSearch;
+    private String chose, input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
+        t = (TextView) findViewById(R.id.text);
+
+        rgroup = (RadioGroup) findViewById(R.id.group);
+        btnSearch = (Button) findViewById(R.id.btnGoToSearch);
+        searchInput = (TextView) findViewById(R.id.tvSearch);
+
+
+        btnSearch.setOnClickListener(go);
+        rgroup.setOnCheckedChangeListener(listener);
     }
+    private Button.OnClickListener go = new Button.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            t.setText("enter ");
+
+        }
+    };
+    private RadioGroup.OnCheckedChangeListener listener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            int p = group.indexOfChild((RadioButton) findViewById(checkedId));
+            int count = group.getChildCount();
+            switch (checkedId) {
+                case R.id.rbName:
+                    t.setText("you select name");
+                    chose = "pname";
+                    break;
+                case R.id.rbBrand:
+                    t.setText("you select brand");
+                    chose = "brand";
+                    break;
+                case R.id.rbPrice:
+                    t.setText("you select price");
+                    chose = "price";
+                    break;
+                case R.id.rbSize:
+                    t.setText("you select size");
+                    chose = "size";
+                    break;
+                case R.id.rbType:
+                    t.setText("you select type");
+                    chose = "ptype";
+                    break;
+
+
+
+            }
+
+        }
+
+    };
 }
